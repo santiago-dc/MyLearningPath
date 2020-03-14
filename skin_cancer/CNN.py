f56db6ff5cd0b411bad3307c523bc64e5f6feafd
santiago-dc <santiago.dominguezc@outlook.es>
2020-03-14 17:23:34 +0100

    Update CNN.py

--- skin_cancer/CNN.py
+++ skin_cancer/CNN.py
@@ -50,131 +50,76 @@ np.random.shuffle(s)
 X_test = X_test[s]
 Y_test = Y_test[s]
 
-# Display first 15 images of moles, and how they are classified
-w=40
-h=30
-fig=plt.figure(figsize=(12, 8))
-columns = 5
-rows = 3
+X =np.concatenate((X_train, X_test), axis = 0)
+Y =np.concatenate((Y_train, Y_test), axis = 0)
+s = np.arange(X.shape[0])
+np.random.shuffle(s)
+X = X[s]
+Y = Y[s]
+X_conv_train = X[0:3000]#2637
+X_conv_test = X[3000:]
+Y_conv_train = Y[0:3000]
+Y_conv_test = Y[3000:]
 
-for i in range(1, columns*rows +1):
-    ax = fig.add_subplot(rows, columns, i)
-    if Y_train[i] == 0:
-        ax.title.set_text('Benign')
-    else:
-        ax.title.set_text('Malignant')
-    plt.imshow(X_train[i],interpolation='nearest')
-plt.show()
+
+# Display first 15 images of moles, and how they are classified
+# w=40
+# h=30
+# fig=plt.figure(figsize=(12, 8))
+# columns = 5
+# rows = 3
+
+# for i in range(1, columns*rows +1):
+#     ax = fig.add_subplot(rows, columns, i)
+#     if Y_train[i] == 0:
+#         ax.title.set_text('Benign')
+#     else:
+#         ax.title.set_text('Malignant')
+#     plt.imshow(X_train[i],interpolation='nearest')
+# plt.show()
 
 #Turn labels into one hot encoding (ya veremos si esto hace falta o si lo dejo)
-Y_train = to_categorical(Y_train, num_classes= 2)
-Y_test = to_categorical(Y_test, num_classes= 2)
+Y_conv_train = to_categorical(Y_conv_train, num_classes= 2)
+Y_conv_test = to_categorical(Y_conv_test, num_classes= 2)
 
 # Normalization
-X_train = X_train/255.
-X_test = X_test/255.
+X_conv_train = X_conv_train/255.
+X_conv_test = X_conv_test/255.
 
 #Model
-from keras.applications.resnet50 import ResNet50
-from keras.optimizers import Adam
-
-input_shape = (224,224,3)
-lr = 1e-5
-epochs = 50
-#batch_size = 64
-
-from tensorflow import keras 
 from keras.models import Sequential
-from keras.layers import Dense, Conv2D, Flatten, MaxPooling2D, AveragePooling2D
-
-# model = Sequential() #Test Acc:  0.7757575511932373
-# #add model layers
-# model.add(Conv2D(64, kernel_size=3, activation='relu', input_shape=(224,224,3)))#capa de 64 filtros de 3x3. dims 64*28*28*1
-# model.add(MaxPooling2D(pool_size=(2, 2), strides=2, padding='same', data_format=None))
-# model.add(Conv2D(32, kernel_size=3, activation='relu'))#capa de 32 filtros de 3x3. dims 32*28*28*1
-# model.add(MaxPooling2D(pool_size=(2, 2), strides=2, padding='same', data_format=None))
-# model.add(Flatten())# pasa los resultados de matriz a vector
-# model.add(Dense(30, activation='relu'))
-# model.add(Dense(2, activation='softmax'))
-
-# model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-
-# model.fit(X_train, Y_train, validation_data=(X_test, Y_test), batch_size=32, epochs=3)
-
-# test_loss, test_acc = model.evaluate(X_test, Y_test)
-# print('Test Acc: ', test_acc)
-
-
-
-# model2 = Sequential() #Test Acc:  0.760606050491333
-# #add model layers
-# model2.add(Conv2D(128, kernel_size=5, activation='relu', input_shape=(224,224,3)))
-# model2.add(MaxPooling2D(pool_size=(2, 2), strides=2, padding='same', data_format=None))
-# #model2.add(Conv2D(128, kernel_size=5, activation='relu'))
-# #model2.add(MaxPooling2D(pool_size=(2, 2), strides=2, padding='same', data_format=None))
-# #model2.add(Conv2D(64, kernel_size=5, activation='relu'))
-# #model2.add(MaxPooling2D(pool_size=(2, 2), strides=2, padding='same', data_format=None))
-# model2.add(Flatten())
-# model2.add(Dense(30, activation='relu'))
-# model2.add(Dense(2, activation='softmax'))
-
-# model2.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-
-# model2.fit(X_train, Y_train, validation_data=(X_test, Y_test), batch_size=16, epochs=5)
-
-# test_loss, test_acc = model2.evaluate(X_test, Y_test, batch_size=16)
-# print('Test Acc: ', test_acc)
-
-
-
-#model3 = Sequential() #Test Acc:  0.5454545617103577
-#add model layers
-#model3.add(Conv2D(128, kernel_size=5, activation='relu', input_shape=(224,224,3)))
-#model3.add(AveragePooling2D(pool_size=(2, 2), strides=2, padding='same', data_format=None))
-#model3.add(Conv2D(128, kernel_size=5, activation='relu'))
-#model3.add(AveragePooling2D(pool_size=(2, 2), strides=2, padding='same', data_format=None))
-#model3.add(Conv2D(64, kernel_size=5, activation='relu'))
-#model3.add(AveragePooling2D(pool_size=(2, 2), strides=2, padding='same', data_format=None))
-#model3.add(Flatten())
-#model3.add(Dense(30, activation='relu'))
-#model3.add(Dense(2, activation='softmax'))
-
-#model3.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-
-#model3.fit(X_train, Y_train, validation_data=(X_test, Y_test), batch_size=16, epochs=3)
-#test_loss, test_acc = model3.evaluate(X_test, Y_test, batch_size=16)
-#print('Test Acc: ', test_acc)
-
 from keras.layers import Dense, Conv2D, Flatten, MaxPooling2D, AveragePooling2D, BatchNormalization, Dropout
 model = Sequential()
-model.add(Conv2D(128, kernel_size=7, activation='relu', input_shape=(224,224,3)))
+model.add(Conv2D(64, kernel_size=7, activation='relu', input_shape=(224,224,3)))
 model.add(MaxPooling2D(pool_size=(2, 2), strides=2, padding='same', data_format=None))
 model.add(BatchNormalization())
-model.add(Dropout(0.25))
-
+#model.add(Dropout(0.25))
 
 model.add(Conv2D(128, kernel_size=5, activation='relu'))
 model.add(MaxPooling2D(pool_size=(2, 2), strides=2, padding='same', data_format=None))
 model.add(BatchNormalization())
-model.add(Dropout(0.25))
+#model.add(Dropout(0.25))
 
 model.add(Conv2D(64, kernel_size=3, activation='relu'))
 model.add(MaxPooling2D(pool_size=(2, 2), strides=2, padding='same', data_format=None))
 model.add(BatchNormalization())
-model.add(Dropout(0.25))
-    
+#model.add(Dropout(0.25))
+from keras import regularizers  
 model.add(Flatten())
-model.add(Dropout(0.25))
-model.add(Dense(125, activation='relu'))
-model.add(Dropout(0.25))
-model.add(Dense(30, activation='relu'))
-model.add(Dense(2, activation='softmax'))  
-
+#model.add(Dropout(0.25))
+model.add(Dense(450, activation='relu',kernel_regularizer=regularizers.l2(0.01)))
+#model.add(Dropout(0.25))
+model.add(Dense(250, activation='relu',kernel_regularizer=regularizers.l2(0.01)))
+#model.add(Dropout(0.25))
+model.add(Dense(125, activation='relu',kernel_regularizer=regularizers.l2(0.01)))
+#model.add(Dropout(0.25))
+model.add(Dense(30, activation='relu',kernel_regularizer=regularizers.l2(0.01)))
+model.add(Dense(2, activation='softmax')) 
 
 
 model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
-model.fit(X_train, Y_train, batch_size=128, epochs=3)
-test_loss, test_acc = model.evaluate(X_test, Y_test, batch_size=16)
+model.fit(X_conv_train, Y_conv_train, validation_data=(X_conv_test, Y_conv_test), batch_size=10, epochs=50)
+test_loss, test_acc = model.evaluate(X_conv_test, Y_conv_test, batch_size=10)
 print('Test Acc: ', test_acc)
 print(':)')

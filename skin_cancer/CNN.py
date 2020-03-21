2f6c8180716e0bc474cb59f6a09e7a20d85d701a
santiago-dc <santiago.dominguezc@outlook.es>
2020-03-21 19:36:57 +0100

    backup

--- skin_cancer/CNN.py
+++ skin_cancer/CNN.py
@@ -50,16 +50,16 @@ np.random.shuffle(s)
 X_test = X_test[s]
 Y_test = Y_test[s]
 
-X =np.concatenate((X_train, X_test), axis = 0)
-Y =np.concatenate((Y_train, Y_test), axis = 0)
-s = np.arange(X.shape[0])
-np.random.shuffle(s)
-X = X[s]
-Y = Y[s]
-X_conv_train = X[0:3000]#2637
-X_conv_test = X[3000:]
-Y_conv_train = Y[0:3000]
-Y_conv_test = Y[3000:]
+# X =np.concatenate((X_train, X_test), axis = 0)
+# Y =np.concatenate((Y_train, Y_test), axis = 0)
+# s = np.arange(X.shape[0])
+# np.random.shuffle(s)
+# X = X[s]
+# Y = Y[s]
+# X_conv_train = X[0:3000]#2637
+# X_conv_test = X[3000:]
+# Y_conv_train = Y[0:3000]
+# Y_conv_test = Y[3000:]
 
 
 # Display first 15 images of moles, and how they are classified
@@ -79,47 +79,47 @@ Y_conv_test = Y[3000:]
 # plt.show()
 
 #Turn labels into one hot encoding (ya veremos si esto hace falta o si lo dejo)
-Y_conv_train = to_categorical(Y_conv_train, num_classes= 2)
-Y_conv_test = to_categorical(Y_conv_test, num_classes= 2)
+Y_train = to_categorical(Y_train, num_classes= 2)
+Y_test = to_categorical(Y_test, num_classes= 2)
 
 # Normalization
-X_conv_train = X_conv_train/255.
-X_conv_test = X_conv_test/255.
+X_train = X_train/255.
+X_test = X_test/255.
 
 #Model
 from keras.models import Sequential
 from keras.layers import Dense, Conv2D, Flatten, MaxPooling2D, AveragePooling2D, BatchNormalization, Dropout
 model = Sequential()
-model.add(Conv2D(64, kernel_size=7, activation='relu', input_shape=(224,224,3)))
+model.add(Conv2D(7, kernel_size=3,padding='same', strides=2, activation='relu', input_shape=(224,224,3)))
 model.add(MaxPooling2D(pool_size=(2, 2), strides=2, padding='same', data_format=None))
-model.add(BatchNormalization())
+#model.add(BatchNormalization())
 #model.add(Dropout(0.25))
 
-model.add(Conv2D(128, kernel_size=5, activation='relu'))
-model.add(MaxPooling2D(pool_size=(2, 2), strides=2, padding='same', data_format=None))
-model.add(BatchNormalization())
+# model.add(Conv2D(3, kernel_size=3,padding='same', strides=2, activation='relu'))
+# model.add(MaxPooling2D(pool_size=(2, 2), strides=2, padding='same', data_format=None))
+# model.add(BatchNormalization())
 #model.add(Dropout(0.25))
 
-model.add(Conv2D(64, kernel_size=3, activation='relu'))
-model.add(MaxPooling2D(pool_size=(2, 2), strides=2, padding='same', data_format=None))
-model.add(BatchNormalization())
+# model.add(Conv2D(64, kernel_size=3, activation='relu'))
+# model.add(MaxPooling2D(pool_size=(2, 2), strides=2, padding='same', data_format=None))
+# model.add(BatchNormalization())
 #model.add(Dropout(0.25))
 from keras import regularizers  
 model.add(Flatten())
 #model.add(Dropout(0.25))
-model.add(Dense(450, activation='relu',kernel_regularizer=regularizers.l2(0.01)))
-#model.add(Dropout(0.25))
-model.add(Dense(250, activation='relu',kernel_regularizer=regularizers.l2(0.01)))
-#model.add(Dropout(0.25))
-model.add(Dense(125, activation='relu',kernel_regularizer=regularizers.l2(0.01)))
-#model.add(Dropout(0.25))
+# model.add(Dense(450, activation='relu',kernel_regularizer=regularizers.l2(0.01)))
+# model.add(Dropout(0.25))
+model.add(Dense(200, activation='relu',kernel_regularizer=regularizers.l2(0.01)))
+model.add(Dropout(0.25))
+model.add(Dense(100, activation='relu',kernel_regularizer=regularizers.l2(0.01)))
+model.add(Dropout(0.25))
 model.add(Dense(30, activation='relu',kernel_regularizer=regularizers.l2(0.01)))
 model.add(Dense(2, activation='softmax')) 
 
 
 model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
-model.fit(X_conv_train, Y_conv_train, validation_data=(X_conv_test, Y_conv_test), batch_size=10, epochs=50)
-test_loss, test_acc = model.evaluate(X_conv_test, Y_conv_test, batch_size=10)
+model.fit(X_train, Y_train, validation_data=(X_test, Y_test), batch_size=10, epochs=100)
+test_loss, test_acc = model.evaluate(X_test, Y_test, batch_size=10)
 print('Test Acc: ', test_acc)
 print(':)')
